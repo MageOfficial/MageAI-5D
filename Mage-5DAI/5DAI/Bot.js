6ffc30d94d9e9ac7dcdb760725a4cb7f55fef6f3
Max Brueggemann <mbrueggemann1214@gmail.com>
2021-06-09 21:33:39 -0400

    Added Killer Heuristic about 2x as fast in certain positions

--- Mage-5DAI/5DAI/Bot.js
+++ Mage-5DAI/5DAI/Bot.js
@@ -22,91 +22,102 @@ var moveValues = [ //Piece Movement Value
     1 / 8,  //king     1/8
 ];
 
-var global = 0;
+//var str1 =
+//    '[Board "Custom"]\n' +
+//    '[Mode "5D"]\n' +
+//    '[3r*1r*k*1/4p*p*bp*/1p*p*1q1p*Q/p*1P*p*4/1P*6/P*2BP*1NP*/3P*1P*P*1/R*4R*K*1:0:1:w]';
+//chess.import(str1);
+
+/*
+chess.move('e3');
+chess.submit();
+chess.move('e5');
+chess.submit();
+chess.move('Qh5');
+chess.submit();
+chess.move('d6');
+chess.submit();
+chess.move('Kd1');
+chess.submit();
+chess.move('a6');
+chess.submit();
+chess.move('Nc3');
+chess.submit();
+chess.move('Bg4');
+chess.submit();
+*/
+
+
+global = 0;
 var isTurnZero = chess.rawBoard.length > 0 ? (chess.rawBoard[0].length > 0 ? chess.rawBoard[0][0] === null : false) : false;
 chess.print();
 
-//evaluate(chess);
+evaluate(chess);
+
 //evaluation
 function evaluate(chess) {
+
     global += 1; // Counts evaluated boards
     var board = chess.board;
     var player = board.player;
     var currTimeline = board.timelines[0];
     var currTurn = currTimeline.turns[currTimeline.turns.length - 1];
     var eval = 0;
-
+    var timelineNum = currTimeline.timeline;
+    var turnNum = currTimeline.turns.length - !isTurnZero;
     //Material
-    for (var i = 0; i < currTurn.pieces.length; i++) { //loop through pieces
-        var currPiece = currTurn.pieces[i];  //current piece in loop
+    for (var i = 0; i < currTurn.pieces.length; i++) {
+        // Current piece to evaluate.
+        var currPiece = currTurn.pieces[i];
         var playerSign = (currPiece.player === player) ? 1 : -1;
+        var currLocation = [timelineNum, turnNum, currPiece.position.rank - 1, currPiece.position.file - 1];
+        var numMoves = chess.raw.pieceFuncs.moves(chess.rawBoard, currLocation, 1, chess.rawPromotionPieces).length;
+
         switch (currPiece.piece) {
-            case '':        //Pawn
-                eval += playerSign * matValues[0];
+            case '': // Pawn.
+                eval += playerSign * (matValues[0] + numMoves * moveValues[0]);
                 break;
-            case 'B':        //Bishop
-                eval += playerSign * matValues[1];
+            case 'B': // Bishop.
+                // Location and number of possible moves.
+                eval += playerSign * (matValues[1] + numMoves * moveValues[1]);
                 break;
-            case 'N':        //Knight
-                eval += playerSign * matValues[2];
+            case 'N': // Knight.
+                // Location and number of possible moves.
+                eval += playerSign * (matValues[2] + numMoves * moveValues[2]);
                 break;
-            case 'B':        //Rook
-                eval += playerSign * matValues[3];
+            case 'R': // Rook.
+                // Location and number of possible moves.
+                eval += playerSign * (matValues[3] + numMoves * moveValues[3]);
                 break;
-            case 'Q':        //Queen
-                eval += playerSign * matValues[4];
+            case 'Q': // Queen.
+                // Location and number of possible moves.
+                eval += playerSign * (matValues[4] + numMoves * moveValues[4]);
                 break;
-            case 'K':        //King
-                eval += playerSign * matValues[5];
-                //King Safety
-                //Find king's exposure squares
-                var kPos = [currTimeline.turns.length -!isTurnZero,currPiece.position.rank - 1, currPiece.position.file - 1];
-                var exposedSq = 0;
-                //var kingPawn = [];
+            case 'K': // King.
+                // Location and number of possible moves.
+                eval += playerSign * (matValues[5] + numMoves * moveValues[5]);
+
+                // Evaluate king safety and find king's pieces.
                 var origVec = chess.raw.pieceFuncs.moveVecs(10);
+                var exposedSq = 0;
+                var kingPawn = []; // currently unused
+
+                // Loop through the eight directions.
                 for (var v = 0; v < 8; v++) {
-
-                    var exposure = kingExposure(chess, origVec[v], kPos, 1);
-
+                    var exposure = kingExposure(chess, origVec[v], currLocation, 1);
+                    /*
+                    if (exposure.piecePos !== undefined) {
+                         kingPawn.push(exposure.piecePos);
+                    }
+                    */
                     exposedSq += exposure.squares;
                 }
+
+                // Decrease the evaluation if there are more than 4 exposed squares.
                 if (exposedSq > 4) {
-                    eval -= playerSign * (exposedSq - 5) * 1
+                    eval -= playerSign * (exposedSq - 5);
                 }
                 break;
-        }
-    }
-
-    //Movement
-    var isPlayerBlack = (player == 'black');
-    var possibleMoves1 = chess.raw.boardFuncs.moves(chess.rawBoard, chess.rawAction, 1, 1, 1, chess.rawPromotionPieces);
-    var tempChess = chess.copy();
-
-    //pass
-    tempChess.raw.mateFuncs.blankAction(tempChess.rawBoard, tempChess.rawAction);
-    tempChess.rawAction++;
-
-    var possibleMoves2 = tempChess.raw.boardFuncs.moves(tempChess.rawBoard, tempChess.rawAction, 1, 1, 1, tempChess.rawPromotionPieces);
-
-    if (isPlayerBlack) {
-        for (var m = 0; m < possibleMoves1.length; m++) {
-            var pieceNum = chess.rawBoard[possibleMoves1[m][0][0]][possibleMoves1[m][0][1]][possibleMoves1[m][0][2]][possibleMoves1[m][0][3]];
-            eval += moveValues[(Math.abs(pieceNum) / 2) - .5];
-        }
-
-        for (var m = 0; m < possibleMoves2.length; m++) {
-            pieceNum = tempChess.rawBoard[possibleMoves2[m][0][0]][possibleMoves2[m][0][1]][possibleMoves2[m][0][2]][possibleMoves2[m][0][3]];
-            eval -= moveValues[(Math.abs(pieceNum) / 2) - 1];
-        }
-    }
-    else {
-        for (var m = 0; m < possibleMoves1.length; m++) {
-            var pieceNum = chess.rawBoard[possibleMoves1[m][0][0]][possibleMoves1[m][0][1]][possibleMoves1[m][0][2]][possibleMoves1[m][0][3]];
-            eval += moveValues[(Math.abs(pieceNum) / 2) - 1];
-        }
-        for (var m = 0; m < possibleMoves2.length; m++) {
-            pieceNum = tempChess.rawBoard[possibleMoves2[m][0][0]][possibleMoves2[m][0][1]][possibleMoves2[m][0][2]][possibleMoves2[m][0][3]];
-            eval -= moveValues[(Math.abs(pieceNum) / 2) - .5];
         }
     }
     console.log('Evaluation = ' + eval);
@@ -114,17 +125,17 @@ function evaluate(chess) {
 }
 
 function kingExposure(chess, moveVec, kPos, mult) {
-    var rPos = kPos[1] + mult * moveVec[2];
-    var fPos = kPos[2] + mult * moveVec[3];
+    var rPos = kPos[2] + mult * moveVec[2];
+    var fPos = kPos[3] + mult * moveVec[3];
     if (rPos < 0 || fPos < 0 || rPos > 7 || fPos > 7) {
         return {
             piecePos: undefined,
             squares: 0,
         };
     }
-    if (chess.rawBoard[0][kPos[0]][rPos][fPos] !== 0) {
+    if (chess.rawBoard[kPos[0]][kPos[1]][rPos][fPos] !== 0) {
         return {
-            piecePos: [0, kPos[0], rPos, fPos],
+            piecePos: [kPos[0], kPos[1], rPos, fPos],
             squares: 0,
         };
     }
@@ -140,58 +151,79 @@ console.log("\nnegaMax\n");
 //Initial Variables
 var alpha = -Infinity;
 var beta = Infinity;
-var depth = 3;
+var depth = 6;
+var killer;
 
 console.time("Evaluation with Depth = " + depth);
-var totalEval = negaMax(chess, depth, alpha, beta); //Function call
+var totalEval = negaMax(chess, depth, alpha, beta, killer); //Function call
 console.timeEnd("Evaluation with Depth = " + depth);
 chess.print();
-console.log("Move = "+ chess.raw.pgnFuncs.fromMove(totalEval.evalMove, chess.rawBoard, chess.rawAction) + ' : '+ Math.round(totalEval.evalMax * 1000) / 1000);
+console.log(totalEval.evalMove)
+console.log("Move = " + chess.raw.pgnFuncs.fromMove(totalEval.evalMove, chess.rawBoard, chess.rawAction) + ' : ' + Math.round(totalEval.eval * 1000) / 1000);
 
+function negaMax(chess, depth, alpha, beta, killer) {
 
-function negaMax(chess, depth, alpha, beta) {
-    
     if (depth == 0) {// Depth limit
 
         //chess.print()
 
         return {
-            evalMax: evaluate(chess),
+            eval: evaluate(chess),
             evalMove: [],
+            killerMove: [],
         };
     }
 
     var possibleMoves = chess.raw.boardFuncs.moves(chess.rawBoard, chess.rawAction, 1, 1, 1, chess.rawPromotionPieces);
 
-    var moveAmount = possibleMoves.length; // Gets amount of moves
-
-    var validMoves = 0;
-    for (var m = 0, count = moveAmount; m < count; m++) {    // Loops through the index of each move
-        var tempChess = chess.copy(); // copies the board state
-
+    for (var m = 0; m < possibleMoves.length; m++) {
+        var tempChess = chess.copy();
         tempChess.raw.boardFuncs.move(tempChess.rawBoard, possibleMoves[m]);
-
-        if (!tempChess.inCheck) {
-            validMoves += 1;
-            tempChess.rawAction++;
-
-            var score = -negaMax(tempChess, depth - 1, -beta, -alpha).evalMax; // Calls the function again to get the score and searches to the final depth
-
-            if (score > alpha) { // records max value
-                alpha = score;
-                var maxMove = possibleMoves[m];
-            }
-            if (score >= beta) {
-                break;
-            }
+        if (tempChess.inCheck) {
+            possibleMoves.splice(m, 1);
+            m--;
         }
     }
-    if (validMoves == 0 && chess.inCheck) {
-        alpha = 0;
+    
+
+    if (killer !== undefined) {
+        var index = possibleMoves.findIndex(killerHeuristic, killer)
+        if (index !== -1) {
+            possibleMoves.splice(index, 1);
+            possibleMoves.unshift(killer);
+        }
     }
 
+    var validMoveAmount = possibleMoves.length;
+    for (var v = 0; v < validMoveAmount; v++) {
+        var nodeChess = chess.copy();
+        nodeChess.raw.boardFuncs.move(nodeChess.rawBoard, possibleMoves[v]);
+        nodeChess.rawAction++;
+        var out = negaMax(nodeChess, depth - 1, -beta, -alpha, killer)
+        score = -out.eval
+        killer=out.killerMove
+
+        if (score > alpha) {
+            alpha = score;
+            var alphaMove = possibleMoves[v];
+        }
+        if (score >= beta) {
+            var betaMove = possibleMoves[v];
+            break;
+        }
+    }
+
+
     return {
-        evalMax: alpha,
-        evalMove: maxMove,
+        eval: alpha,
+        evalMove: alphaMove,
+        killerMove: betaMove,
     };
 }
+
+function killerHeuristic(killerMove) {
+    return killerMove[0][2] == this[0][2] && killerMove[0][3] == this[0][3] && killerMove[1][2] == this[1][2] && killerMove[1][3] == this[1][3];
+}
+
+
+console.log("Postions Checked = " + global);
